e8ef17aff35436785fc15e0294ff07d1b5d7e72e
Myke Atkinson <ploe@hotmail.co.uk>
2018-10-17 15:08:50 +0100

    [WIP] Made a sprite appear! Structure needs tidying though.

--- main.asm
+++ main.asm
@@ -24,21 +24,6 @@ Start:
 	xor a
 	ld [rLCDC], a
 
-	ld hl, $8000
-	ld de, DAISY_SPRITESHEET
-	ld bc, DAISY_SPRITESHEET_END
-.copyDaisy
-	; copy byte and increment counters
-	ld a, [de]
-	ld [hli], a
-	inc de
-	dec bc
-	
-	; loop if count is non-zero
-	ld a, b
-	or c
-	jr nz, .copyDaisy
-; end of copyDaisy
 
 	; initialize copyFont loop
 	ld hl, $9000
@@ -56,6 +41,23 @@ Start:
 	or c
 	jr nz, .copyFont
 ; end of copyFont
+
+	ld hl, $8000
+	ld de, DAISY_SPRITESHEET
+	ld bc, DAISY_SPRITESHEET_END - DAISY_SPRITESHEET
+.copyDaisy
+	; copy byte and increment counters
+	ld a, [de]
+	ld [hli], a
+	inc de
+	dec bc
+	
+	; loop if count is non-zero
+	ld a, b
+	or c
+	jr nz, .copyDaisy
+; end of copyDaisy
+
 
 	; copy string to top left corner
 	ld hl, $9800
@@ -80,12 +82,53 @@ Start:
 	; turn off the sound
 	ld [rNR52], a
 
+	ld a, 24
+	ld [OAM_BUFFER], a
+
+	ld a, 32
+	ld [OAM_BUFFER+1], a
+
 	; turn screen on, show background
-	ld a, %10000001
+	ld a, %10000011
 	ld [rLCDC], a
+
+	call DMA_COPY_IDLE
+	call $FF80
 
 .lockup
 	jr .lockup
+
+DMA_IDLE:
+	ld a, $C1
+	ld [rDMA], a
+
+	ld a, $28
+.next
+	dec a
+	jr nz, .next
+	ret
+
+DMA_IDLE_END:
+	
+
+DMA_COPY_IDLE:
+	ld hl, $FF80	; dst
+	ld de, DMA_IDLE	; iterator 
+	ld bc, DMA_IDLE_END - DMA_IDLE 
+
+.next
+	; copy byte and increment counters
+	ld a, [de]
+	ld [hli], a
+	inc de
+	dec bc
+	
+	; loop if count is non-zero
+	ld a, b
+	or c
+	jr nz, .next
+
+	ret
 
 SECTION "Font", ROM0
 
@@ -100,4 +143,7 @@ DAISY_SPRITESHEET_END:
 Section "Hello World string", ROM0
 
 HelloWorldStr:
-	db "hello, world", 0
+	db "hello, world",0
+
+SECTION "OAM Buffer", WRAM0[$C100]
+OAM_BUFFER: DS 4*40
